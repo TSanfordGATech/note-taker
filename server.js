1ad8c3413ebd6177917a4b559588340306063188
Tori Sanford <tori.danielle.clark91@gmail.com>
2021-03-06 20:40:03 -0500

    added post request

--- server.js
+++ server.js
@@ -23,9 +23,24 @@ app.use(express.static("./develop/public"));
 
 // GET request
 app.get("/api/notes", function(req, res) {
-    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+    readFileAsync("./Develop/db/db.json", "utf8").then(function(data) {
         notes = [].concat(JSON.parse(data))
         res.json(notes);
     })
+});
+
+// POST 
+app.post("/api/notes", function(req,res) {
+    const note = req.body;
+    readFileAsync("./Develop/db/db.json", "utf8").then(function(data) {
+    const notes = [].concat(JSON.parse(data));
+    note.id = notes.length + 1
+    notes.push(note);
+    return notes
+})
+.then(function(notes) {
+    writeFileAsync("./Develop/db/db.json", JSON.stringify(notes))
+    res.json(note);
+})
 });
 
